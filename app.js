18935b217e16a449bc84f518605f8c03c24adc43
Galanyksa1@gmail.com <Galanyksa1@gmail.com>
2021-09-06 23:48:35 +0300

    check operation end number

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
-const calc = function () {
+function calc() {
+
   let number1;
   let number2;
   let operate;
 
   const setOperate = function () {
-    return prompt('Введите операцию (+, -, /, *.)');
+    const operation = prompt('Enter operation (+, -, /, *.)');
+    console.log(operation);
+    if(operation === '+' || operation === '-' ||
+       operation === '/' || operation === '*')
+       {
+          return operation;
+       }
+    alert('Wrong operation');
   }
 
   const setNum = function () {
-    return prompt('Введите число');
+    const result = prompt('Enter number');
+    console.log(result);
+
+    if(isNaN(result))
+    {
+      alert('Wrong value');
+    }
+
+    if(result === null) {
+      return 0;
+    }
+    return result;
   }
 
   const resultatOperation = function (operantL1, operantR2, operate) {
-    console.log(operantL1, operantR2)
     let result;
 
     if (operate === '+') {
-      result = operantL1 + operantR2 ;
+      result = operantL1 + operantR2;
     }
     else if (operate === '-') {
-      result = operantL1 - operantR2 ;
+      result = operantL1 - operantR2;
     }
     else if (operate === '/') {
-      result = operantL1 / operantR2 ;
+      if(operantR2 === 0)
+      {
+        alert('Wrong operation');
+        return;
+      }
+      result = operantL1 / operantR2;
     }
     else if (operate === '*') {
-      result = operantL1 * operantR2 ;
-    }  
-    
+      result = operantL1 * operantR2;
+    }
+
     return result;
   }
 
@@ -35,9 +58,10 @@ const calc = function () {
   number1 = setNum();
   number2 = setNum();
 
-  const resultat  = resultatOperation (Number(number1) ,Number(number2), operate);
-  console.log(operate, number1, number2);
+  const resultat = resultatOperation(Number(number1), Number(number2), operate);
+  console.log(resultat);
+
   alert(resultat);
 }
 
-calc ();
+calc();
